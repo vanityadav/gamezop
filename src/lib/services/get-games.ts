244373108a726090a0ca72cbd1fd43c2964d0f99
Vanit Yadav <vanit.yadav@revnue.com>
2024-02-06 02:48:42 +0530

    fixes: deploy test

--- src/lib/services/get-games.ts
+++ src/lib/services/get-games.ts
@@ -5,23 +5,23 @@ type GamesApiResponse = {
   games: Game[];
 };
 
-// const getGames = async () => {
-//   try {
-//     const response = await fetch(process.env.API_URL);
+const getGames = async () => {
+  try {
+    const response = await fetch(process.env.API_URL);
 
-//     if (!response.ok) {
-//       throw new Error("Service Error");
-//     }
+    if (!response.ok) {
+      throw new Error("Service Error");
+    }
 
-//     const games = (await response.json()) as GamesApiResponse;
-//     return games?.games;
-//   } catch (error) {
-//     throw new Error("Oh, There was an Error");
-//   }
-// };
-
-const getGames = (): Game[] => {
-  return games?.games;
+    const games = (await response.json()) as GamesApiResponse;
+    return games?.games;
+  } catch (error) {
+    throw new Error("Oh, There was an Error");
+  }
 };
+
+// const getGames = (): Game[] => {
+//   return games?.games;
+// };
 
 export default getGames;

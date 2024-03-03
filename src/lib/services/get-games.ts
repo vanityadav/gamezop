de679e912bd046f0185c0bc8424649afd415e4c7
Vanit Yadav <110538930+vanityadav@users.noreply.github.com>
2024-03-04 00:23:04 +0530

    Update get-games.ts

--- src/lib/services/get-games.ts
+++ src/lib/services/get-games.ts
@@ -6,25 +6,25 @@ type GamesApiResponse = {
 
 
 // for prod
-const getGames = async () => {
-  try {
-    const response = await fetch(process.env.API_URL, { cache: "no-store" });
+// const getGames = async () => {
+//   try {
+//     const response = await fetch(process.env.API_URL, { cache: "no-store" });
 
-    if (!response.ok) {
-      throw new Error("Service Error");
-    }
+//     if (!response.ok) {
+//       throw new Error("Service Error");
+//     }
 
-    const games = (await response.json()) as GamesApiResponse;
-    return games?.games;
-  } catch (error) {
-    throw new Error("Oh, There was an Error");
-  }
-};
+//     const games = (await response.json()) as GamesApiResponse;
+//     return games?.games;
+//   } catch (error) {
+//     throw new Error("Oh, There was an Error");
+//   }
+// };
 
 
 // for local
-// const getGames = (): Game[] => {
-//   return games?.games;
-// };
+const getGames = (): Game[] => {
+  return games?.games;
+};
 
 export default getGames;
